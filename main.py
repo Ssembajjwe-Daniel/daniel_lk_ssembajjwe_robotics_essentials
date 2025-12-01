6ab79f3daa0cdd65d95656766315ff9603a5dd2e
Ssembajjwe-Daniel <daniellkssembajjwe@gmail.com>
2025-12-01 17:57:11 +0100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+# music.play(music.string_playable("G B A G C5 B A B ", 120),
+# 
+# music.PlaybackMode.UNTIL_DONE)
+from CuteBot import ultrasonic
+def getmax(a: number, b: number):
+    if b == a:
+        print("b was equal to a")
+    elif b > a:
+        led.plot_bar_graph(a, b)
+    else:
+        cuteBot.stopcar()
+d = 0
+c = 0
+# the basic module provides access to the microbit functionality
+basic.show_icon(IconNames.LEFT_TRIANGLE)
+basic.show_number(12)
+basic.pause(1000)
+basic.show_string("Robotics<..>")
+strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
+strip.show_rainbow(1, 360)
+
+# function call that controls how long the bot moves
+cuteBot.move_time(cuteBot.Direction.FORWARD, 60, 300)
+# cuteBot.Direction.BACKWARD
+# cuteBot.Direction.FORWARD
+getmax(6, 6)
+#the cuteBot.ultrasonic(cuteBot.SonarUnit.CENTIMETERS) returns a value and assigned
+#to the distance variable,then we check if distance is in given range,stop or move left
+
+cuteBot.move_time(cuteBot.Direction.FORWARD, 60, 3000)
+while True:
+    distance = cuteBot.ultrasonic(cuteBot.SonarUnit.CENTIMETERS)
+    
+    if distance < 10:
+        cuteBot.stopcar()
+        basic.show_string("Turning")
+    else:
+        cuteBot.Direction.LEFT
+        cuteBot.motors(30, 90)
--- main.ts
+++ main.ts
@@ -1,3 +1,43 @@
-basic.forever(function on_forever() {
+let distance: number;
+//  music.play(music.string_playable("G B A G C5 B A B ", 120),
+//  
+//  music.PlaybackMode.UNTIL_DONE)
+function getmax(a: number, b: number) {
+    if (b == a) {
+        console.log("b was equal to a")
+    } else if (b > a) {
+        led.plotBarGraph(a, b)
+    } else {
+        cuteBot.stopcar()
+    }
     
-})
+}
+
+let d = 0
+let c = 0
+//  the basic module provides access to the microbit functionality
+basic.showIcon(IconNames.LeftTriangle)
+basic.showNumber(12)
+basic.pause(1000)
+basic.showString("Robotics<..>")
+let strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
+strip.showRainbow(1, 360)
+//  function call that controls how long the bot moves
+cuteBot.moveTime(cuteBot.Direction.forward, 60, 300)
+//  cuteBot.Direction.BACKWARD
+//  cuteBot.Direction.FORWARD
+getmax(6, 6)
+// the cuteBot.ultrasonic(cuteBot.SonarUnit.CENTIMETERS) returns a value and assigned
+// to the distance variable,then we check if distance is in given range,stop or move left
+cuteBot.moveTime(cuteBot.Direction.forward, 60, 3000)
+while (true) {
+    distance = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
+    if (distance < 10) {
+        cuteBot.stopcar()
+        basic.showString("Turning")
+    } else {
+        cuteBot.Direction.left
+        cuteBot.motors(30, 90)
+    }
+    
+}
